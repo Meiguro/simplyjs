9c493d6479ebebe5094cb80d945e6cbfef9502e0
Meiguro <meiguro@meiguro.com>
2013-12-15 03:52:58 -0800

    Add ajax.js

---
+++ src/js/ajax.js
@@ -0,0 +1,59 @@
+/*
+ * ajax.js by Meiguro - MIT License
+ */
+
+var ajax = (function(){
+
+var formify = function(data) {
+  var params = [], i = 0;
+  for (var name in data) {
+    params[i++] = encodeURI(name) + '=' + encodeURI(data[name]);
+  }
+  return params.join('&');
+};
+
+var ajax = function(opt, success, failure) {
+  var method = opt.method || 'GET';
+  var url = opt.url;
+  //console.log(method + ' ' + url);
+
+  var req = new XMLHttpRequest();
+  var headers = opt.headers;
+  if (headers) {
+    for (var name in headers) {
+      req.setRequestHeader(name, headers[name]);
+    }
+  }
+
+  var data = null;
+  if (opt.data) {
+    if (opt.type === 'json') {
+      req.setRequestHeader('Content-Type', 'application/json');
+      data = JSON.stringify(opt.data);
+    } else {
+      data = formify(opt.data);
+    }
+  }
+
+  req.open(method.toUpperCase(), url, !opt.async);
+  req.onload = function(e) {
+    if (req.readyState == 4) {
+      var body = req.responseText;
+      if (opt.type == 'json') {
+        body = JSON.parse(body);
+      }
+      var callback = req.status == 200 ? success : failure;
+      if (callback) {
+        callback(body, req.status, req);
+      }
+    }
+  }
+
+  req.send(data);
+};
+
+ajax.formify = formify;
+
+return ajax;
+
+})();

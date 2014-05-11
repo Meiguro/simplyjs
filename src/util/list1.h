b53d4273ac2c1e22190d7f7b98676ac8515a70cc
Meiguro <meiguro@meiguro.com>
2014-05-11 05:27:23 -0700

    Add util list1 for singly linked lists

---
+++ src/util/list1.h
@@ -0,0 +1,45 @@
+#pragma once
+
+#include <stdlib.h>
+#include <stdbool.h>
+
+typedef struct List1Node List1Node;
+
+struct List1Node {
+  List1Node *next;
+};
+
+typedef bool (*List1FilterCallback)(List1Node *node, void *data);
+
+static inline List1Node *list1_find_prev(List1Node *node,
+    List1FilterCallback callback, void *data, List1Node **prev_out) {
+  for (List1Node *prev = NULL; node; node = node->next) {
+    if (callback(node, data)) {
+      if (prev_out) {
+        *prev_out = prev;
+      }
+      return node;
+    }
+    prev = node;
+  }
+  return NULL;
+}
+
+static inline List1Node *list1_find(List1Node *node, List1FilterCallback callback, void *data) {
+  return list1_find_prev(node, callback, data, NULL);
+}
+
+static inline List1Node *list1_remove_one(List1Node **head, List1FilterCallback callback, void *data) {
+  List1Node *prev = NULL;
+  List1Node *node = *head;
+  node = list1_find_prev(node, callback, data, &prev);
+  if (node) {
+    if (head && *head == node) {
+      *head = node->next;
+    }
+    if (prev) {
+      prev->next = node->next;
+    }
+  }
+  return node;
+}

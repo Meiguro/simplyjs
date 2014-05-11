370d9a10413930be23cf981db4e1526abb2c4535
Meiguro <meiguro@meiguro.com>
2014-05-10 23:45:51 -0700

    Add simply menu section and menu item

--- src/simply_menu.c
+++ src/simply_menu.c
@@ -2,12 +2,33 @@
 
 SimplyMenu *s_menu = NULL;
 
+static SimplyMenuSection *get_menu_section(SimplyMenu *self, int index) {
+  for (SimplyMenuSection *section = self->sections; section; section = section->next) {
+    if (section->index == index) {
+      return section;
+    }
+  }
+  return NULL;
+}
+
+static SimplyMenuItem *get_menu_item(SimplyMenu *self, int section, int index) {
+  for (SimplyMenuItem *item = self->items; item; item = item->next) {
+    if (item->section == section && item->index == index) {
+      return item;
+    }
+  }
+  return NULL;
+}
+
 static uint16_t menu_get_num_sections_callback(MenuLayer *menu_layer, void *data) {
-  return 1;
+  SimplyMenu *self = data;
+  return self->num_sections;
 }
 
 static uint16_t menu_get_num_rows_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
-  return 1;
+  SimplyMenu *self = data;
+  SimplyMenuSection *section = get_menu_section(self, section_index);
+  return section ? section->num_items : 1;
 }
 
 static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t section_index, void *data) {
@@ -15,11 +36,19 @@ static int16_t menu_get_header_height_callback(MenuLayer *menu_layer, uint16_t s
 }
 
 static void menu_draw_header_callback(GContext* ctx, const Layer *cell_layer, uint16_t section_index, void *data) {
+  SimplyMenu *self = data;
+  SimplyMenuSection *section = get_menu_section(self, section_index);
+  if (!section) { return; }
 
+  menu_cell_basic_header_draw(ctx, cell_layer, section->title);
 }
 
 static void menu_draw_row_callback(GContext* ctx, const Layer *cell_layer, MenuIndex *cell_index, void *data) {
+  SimplyMenu *self = data;
+  SimplyMenuItem *item = get_menu_item(self, cell_index->section, cell_index->row);
+  if (!item) { return; }
 
+  menu_cell_basic_draw(ctx, cell_layer, item->title, item->subtitle, NULL);
 }
 
 static void menu_select_callback(MenuLayer *menu_layer, MenuIndex *cell_index, void *data) {
@@ -37,7 +66,7 @@ static void window_load(Window *window) {
   Layer *menu_base_layer = menu_layer_get_layer(menu_layer);
   layer_add_child(window_layer, menu_base_layer);
 
-  menu_layer_set_callbacks(menu_layer, NULL, (MenuLayerCallbacks){
+  menu_layer_set_callbacks(menu_layer, self, (MenuLayerCallbacks){
     .get_num_sections = menu_get_num_sections_callback,
     .get_num_rows = menu_get_num_rows_callback,
     .get_header_height = menu_get_header_height_callback,
--- src/simply_menu.h
+++ src/simply_menu.h
@@ -4,9 +4,31 @@
 
 typedef struct SimplyMenu SimplyMenu;
 
+typedef struct SimplyMenuSection SimplyMenuSection;
+
+typedef struct SimplyMenuItem SimplyMenuItem;
+
 struct SimplyMenu {
   Window *window;
   MenuLayer *menu_layer;
+  SimplyMenuSection *sections;
+  SimplyMenuItem *items;
+  int num_sections;
+};
+
+struct SimplyMenuSection {
+  SimplyMenuSection *next;
+  char *title;
+  int16_t index;
+  int16_t num_items;
+};
+
+struct SimplyMenuItem {
+  SimplyMenuItem *next;
+  char *title;
+  char *subtitle;
+  int16_t section;
+  int16_t index;
 };
 
 SimplyMenu *simply_menu_create(void);

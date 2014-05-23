c70309c0a5c30ec157ac657c51c3c4ee45416abb
Meiguro <meiguro@meiguro.com>
2014-05-22 19:34:48 -0700

    Update simply demo

--- public/demo.js
+++ public/demo.js
@@ -1,23 +1,23 @@
+/* global simply */
 console.log('Simply.js demo!');
 
-simply.on('singleClick', function(e) {
-  console.log(util2.format('single clicked $button!', e));
-  simply.subtitle('Pressed ' + e.button + '!');
-});
-
-simply.on('longClick', function(e) {
-  console.log(util2.format('long clicked $button!', e));
-  simply.vibe();
-  simply.scrollable(e.button !== 'select');
-});
-
-simply.on('accelTap', function(e) {
-  console.log(util2.format('tapped accel axis $axis $direction!', e));
-  simply.subtitle('Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
-});
-
-simply.setText({
+simply.text({
   title: 'Simply Demo!',
   body: 'This is a demo. Press buttons or tap the watch!',
 }, true);
 
+simply.on('singleClick', function(e) {
+  console.log('single clicked ' + e.button + '!');
+  simply.subtitle('Pressed ' + e.button + '!');
+});
+
+simply.on('longClick', function(e) {
+  console.log('long clicked ' + e.button + '!');
+  simply.subtitle('Long pressed ' + e.button + '!');
+  simply.vibe();
+});
+
+simply.on('accelTap', function(e) {
+  console.log('accel tapped axis ' + e.axis + ' ' + e.direction + '!');
+  simply.subtitle('Tapped ' + (e.direction > 0 ? '+' : '-') + e.axis + '!');
+});

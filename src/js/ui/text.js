939ebb20878f9fb860b3f8288bd94b1754d5b97d
Meiguro <meiguro@meiguro.com>
2014-05-26 01:06:50 -0700

    Add text element class

---
+++ src/js/ui/text.js
@@ -0,0 +1,22 @@
+var util2 = require('lib/util2');
+var Propable = require('ui/propable');
+var StageElement = require('ui/element');
+
+var textProps = [
+  'text',
+  'font',
+  'color',
+  'textOverflow',
+  'textAlign',
+];
+
+var Text = function(elementDef) {
+  StageElement.call(this, elementDef);
+  this.state.type = 3;
+};
+
+util2.inherit(Text, StageElement);
+
+Propable.makeAccessors(textProps, Text.prototype);
+
+module.exports = Text;

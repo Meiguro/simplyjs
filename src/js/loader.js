cfefe64b498be2e5cb7cc52c3ffa659e9812dcfb
Meiguro <meiguro@meiguro.com>
2014-05-30 18:31:34 -0400

    Add loader.js paths for attempting to load relative to multiple paths

--- src/js/loader.js
+++ src/js/loader.js
@@ -8,14 +8,10 @@ loader.packagesLinenoOrder = [{ filename: 'loader.js', lineno: 0 }];
 
 loader.extpaths = ['?', '?.js', '?.json', '?/index.js'];
 
-loader.require = function(path) {
-  var module;
-  var extpaths = loader.extpaths;
-  for (var i = 0, ii = extpaths.length; !module && i < ii; ++i) {
-    var filepath = extpaths[i].replace('?', path);
-    module = loader.packages[filepath];
-  }
+loader.paths = ['lib', 'vendor'];
 
+loader.require = function(path) {
+  var module = loader.getPackage(path);
   if (!module) {
     throw new Error("Cannot find module'" + path + "'");
   }
@@ -31,18 +27,38 @@ loader.require = function(path) {
   return module.exports;
 };
 
-loader.define = function(path, lineno, loader) {
+var compareLineno = function(a, b) { return a.lineno - b.lineno; };
+
+loader.define = function(path, lineno, loadfun) {
   var module = {
     filename: path,
     lineno: lineno,
-    loader: loader,
+    loader: loadfun,
   };
 
   loader.packages[path] = module;
   loader.packagesLinenoOrder.push(module);
-  loader.packagesLinenoOrder.sort(function(a, b) {
-    return a.lineno - b.lineno;
-  });
+  loader.packagesLinenoOrder.sort(compareLineno);
+};
+
+loader.getPackage = function(path) {
+  var module = loader.getPackageAtPath(path);
+  var paths = loader.paths;
+  for (var i = 0, ii = paths.length; !module && i < ii; ++i) {
+    var dirpath = paths[i];
+    module = loader.getPackageAtPath(dirpath + '/' + path);
+  }
+  return module;
+};
+
+loader.getPackageAtPath = function(path) {
+  var module;
+  var extpaths = loader.extpaths;
+  for (var i = 0, ii = extpaths.length; !module && i < ii; ++i) {
+    var filepath = extpaths[i].replace('?', path);
+    module = loader.packages[filepath];
+  }
+  return module;
 };
 
 loader.getPackageByLineno = function(lineno) {

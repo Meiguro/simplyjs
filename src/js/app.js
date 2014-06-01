5f8fd1bfd9d8f27558e91c8e82b93c8e430b8df8
Meiguro <meiguro@meiguro.com>
2014-06-01 02:06:31 -0700

    Touch up the default app.js

--- src/js/app.js
+++ src/js/app.js
@@ -1,32 +1,45 @@
-// Welcome to PebbleJS!
-//
-// This is where you write your app.
+/**
+ * Welcome to Pebble.js!
+ *
+ * This is where you write your app.
+ */
 
-var wind = new Pebble.UI.Card({
-  title: "PebbleJS",
-  icon: "images/menu_icon.png",
-  body: "Saying Hello World"
+var UI = require('ui');
+var Vector2 = require('vector2');
+
+var wind = new UI.Card({
+  title: 'Pebble.js',
+  icon: 'images/menu_icon.png',
+  body: 'Saying Hello World'
 });
+
 wind.show();
 
-wind.on('click', function(e) {
-  console.log("Button pressed: " + JSON.stringify(e));
-  if (e.button == 'up') {
-    var menu = new Pebble.UI.Menu();
-    menu.items(0, [ { title: 'Hello World!', subtitle: 'text' }, { title: 'item2' } ]);
-    menu.show();
-  }
-  else if (e.button === 'select') {
-    var stage = new Pebble.UI.Stage();
-    stage.add(new Pebble.UI.Text({ text: 'Yo!', position: new Pebble.UI.Vector2(10, 10), size: new Pebble.UI.Vector2(100, 30) }));
-    stage.show();
-  }
-  else /* 'down' */ {
-    var card = new Pebble.UI.Card();
-    card.title("A PebbleJS Card");
-    card.subtitle("With subtitle");
-    card.body("This is the simplest element you can push to the screen");
-    card.show();
-  }
+wind.on('click', 'up', function(e) {
+  var menu = new UI.Menu();
+  menu.items(0, [{
+    title: 'Hello World!', subtitle: 'Subtitle text'
+  }, {
+    title: 'Item #2'
+  }]);
+  menu.show();
 });
 
+wind.on('click', 'select', function(e) {
+  var stage = new UI.Stage();
+  var textfield = new UI.Text({
+    text: 'Yo!',
+    position: new Vector2(10, 10),
+    size: new Vector2(100, 30),
+  });
+  stage.add(textfield);
+  stage.show();
+});
+
+wind.on('click', 'down', function(e) {
+  var card = new UI.Card();
+  card.title('A Pebble.js Card');
+  card.subtitle('With subtitle');
+  card.body('This is the simplest window you can push to the screen.');
+  card.show();
+});

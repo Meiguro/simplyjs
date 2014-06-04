ada1456d413c0feaa71dfc052bc4626525e97f83
Thomas Sarlandie <thomas@sarlandie.net>
2014-06-04 00:25:14 -0700

    Export Vibe with the UI module

--- src/js/ui/index.js
+++ src/js/ui/index.js
@@ -10,5 +10,6 @@ UI.Text = require('ui/text');
 UI.TimeText = require('ui/timetext');
 UI.Image = require('ui/image');
 UI.Inverter = require('ui/inverter');
+UI.Vibe = require('ui/vibe');
 
 module.exports = UI;

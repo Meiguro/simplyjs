67451abed3f0a788596b4a1b22eaf9418df5de60
Meiguro <meiguro@meiguro.com>
2014-06-18 06:33:52 -0700

    Remove nested test menu from app.js

--- src/js/app.js
+++ src/js/app.js
@@ -22,17 +22,7 @@ main.on('click', 'up', function(e) {
       items: [{
         title: 'Pebble.js',
         icon: 'images/menu_icon.png',
-        subtitle: 'Can do Menus',
-        select: function() {
-          var fMenu = new UI.Menu();
-          fMenu.show();
-          fMenu.item(0, 0, {title: 'one'});
-          fMenu.item(0, 1, {title: 'two'});
-          fMenu.item(0, 2, {title: 'three'});
-          fMenu.on('select', function(e) {
-            console.log(e.item);
-          });
-        }
+        subtitle: 'Can do Menus'
       }, {
         title: 'Second Item',
         subtitle: 'Subtitle Text'

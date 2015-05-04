88efa822c17b1456ed5b52ce20c89b9e6138f709
Meiguro <meiguro@meiguro.com>
2015-05-04 02:42:58 -0700

    Fix inverter layer to draw within the drawing box

--- src/util/inverter_layer.h
+++ src/util/inverter_layer.h
@@ -2,6 +2,8 @@
 
 #include <pebble.h>
 
+#include "util/math.h"
+
 #ifdef PBL_COLOR
 
 typedef struct InverterLayer InverterLayer;
@@ -12,13 +14,22 @@ static void inverter_layer_update_proc(Layer *layer, GContext *ctx) {
   const size_t bytes_per_row = gbitmap_get_bytes_per_row(frame_buffer);
   GColor8 *data = (GColor8 *)gbitmap_get_data(frame_buffer);
 
+  Layer *window_layer = window_get_root_layer(layer_get_window(layer));
+  GRect window_bounds = layer_get_bounds(window_layer);
+  GRect window_frame = layer_get_frame(window_layer);
+  GRect drawing_box = {
+    .origin.x = window_frame.origin.x + window_bounds.origin.x,
+    .origin.y = window_frame.origin.y + window_bounds.origin.y,
+    .size = window_frame.size,
+  };
+
   // This layer must not be nested in a layer not positioned at origin
   GRect frame = layer_get_frame(layer);
 
-  const int16_t min_x = frame.origin.x;
-  const int16_t max_x = frame.size.w + min_x;
-  const int16_t min_y = frame.origin.y;
-  const int16_t max_y = frame.size.h + min_y;
+  const int16_t min_x = MAX(0, drawing_box.origin.x + frame.origin.x);
+  const int16_t min_y = MAX(0, drawing_box.origin.y + frame.origin.y);
+  const int16_t max_x = MIN(drawing_box.size.w, min_x + frame.size.w);
+  const int16_t max_y = MIN(drawing_box.size.h, min_y + frame.size.h);
 
   data += bytes_per_row * min_y;
   for (int16_t y = min_y; y < max_y; y++) {

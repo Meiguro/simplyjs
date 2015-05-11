d851bb1b70bd74bf5b0d55e135509effb6c618e4
Meiguro <meiguro@meiguro.com>
2015-05-11 04:48:43 -0700

    Remove outdated card docs

--- src/js/ui/card.js
+++ src/js/ui/card.js
@@ -6,29 +6,6 @@ var Propable = require('ui/propable');
 var Window = require('ui/window');
 var simply = require('ui/simply');
 
-/**
- * Sets the title field. The title field is the first and largest text field available.
- * @memberOf simply
- * @param {string} text - The desired text to display.
- * @param {boolean} [clear] - If true, all other text fields will be cleared.
- */
-
-/**
- * Sets the subtitle field. The subtitle field is the second large text field available.
- * @memberOf simply
- * @param {string} text - The desired text to display.
- * @param {boolean} [clear] - If true, all other text fields will be cleared.
- */
-
-/**
- * Sets the body field. The body field is the last text field available meant to display large bodies of text.
- * This can be used to display entire text interfaces.
- * You may even clear the title and subtitle fields in order to display more in the body field.
- * @memberOf simply
- * @param {string} text - The desired text to display.
- * @param {boolean} [clear] - If true, all other text fields will be cleared.
- */
-
 var textProps = [
   'title',
   'subtitle',
@@ -46,14 +23,6 @@ var actionProps = [
   'select',
   'back',
 ];
-
-/**
- * Set the Pebble UI style.
- * The available styles are 'small', 'large', and 'mono'. Small and large correspond to the system notification styles.
- * Mono sets a monospace font for the body textfield, enabling more complex text UIs or ASCII art.
- * @memberOf simply
- * @param {string} type - The type of style to set: 'small', 'large', or 'mono'.
- */
 
 var configProps = [
   'style',

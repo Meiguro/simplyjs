d65379f168c569209d073f3473ffcdb859f430b7
Meiguro <meiguro@meiguro.com>
2015-07-08 20:45:21 -0700

    Fix Clock weekday to always return a time in the future

--- src/js/clock/clock.js
+++ src/js/clock/clock.js
@@ -1,5 +1,10 @@
 var Clock = module.exports;
 
 Clock.weekday = function(weekday, hour, minute, seconds) {
-  return moment({ hour: hour, minute: minute, seconds: seconds }).day(weekday).unix();
+  var now = moment();
+  var target = moment({ hour: hour, minute: minute, seconds: seconds }).day(weekday);
+  if (moment.max(now, target) === now) {
+    target.add(1, 'week');
+  }
+  return target.unix();
 };

3c460877f9ad60d48f32259606b4523c4d56c066
Meiguro <meiguro@meiguro.com>
2015-08-19 15:56:36 -0400

    Add moment global usage warning

--- src/js/main.js
+++ src/js/main.js
@@ -6,14 +6,37 @@
  * By default, this will run app.js
  */
 
-require('safe');
+var safe = require('safe');
+var util2 = require('util2');
 
 Pebble.addEventListener('ready', function(e) {
   // Initialize the Pebble protocol
   require('ui/simply-pebble.js').init();
+
   // Backwards compatibility: place moment.js in global scope
   // This will be removed in a future update
-  window.moment = require('vendor/moment');
+  var moment = require('vendor/moment');
+
+  var momentPasser = function(methodName) {
+    return function() {
+      if (safe.warnGlobalMoment !== false) {
+        safe.warn("You've accessed moment globally. Pleae use `var moment = require('moment')` instead.\n\t" +
+                  'moment will not be automatically loaded as a global in future versions.', 5);
+        safe.warnGlobalMoment = false;
+      }
+      return (methodName ? moment[methodName] : moment).apply(this, arguments);
+    };
+  };
+
+  var globalMoment = momentPasser();
+  util2.copy(moment.prototype, globalMoment.prototype);
+  for (var k in moment) {
+    var v = moment[k];
+    globalMoment[k] = typeof v === 'function' ? momentPasser(k) : v;
+  }
+
+  window.moment = globalMoment;
+
   // Load local file
   require('app.js');
 });

5e8e82b221e5528aecbd614a26b62259f4254ab1
Meiguro <meiguro@meiguro.com>
2015-10-21 00:21:07 -0700

    Add SDK IF_ELSE and USAGE macros

--- src/util/sdk.h
+++ src/util/sdk.h
@@ -4,6 +4,14 @@
 
 #ifdef PBL_SDK_3
 #define SDK_SELECT(sdk3, sdk2) sdk3
+#define IF_SDK_3_ELSE(sdk3, other) sdk3
+#define IF_SDK_2_ELSE(sdk2, other) other
+#define SDK_3_USAGE
+#define SDK_2_USAGE __attribute__((unused))
 #elif PBL_SDK_2
 #define SDK_SELECT(sdk3, sdk2) sdk2
+#define IF_SDK_3_ELSE(sdk3, other) other
+#define IF_SDK_2_ELSE(sdk2, other) sdk2
+#define SDK_3_USAGE __attribute__((unused))
+#define SDK_2_USAGE
 #endif

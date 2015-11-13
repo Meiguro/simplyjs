fe1c771e103aa5e3a9fbe42cf4aaf26e1a64c9f2
Cat Haines <cat@pebble.com>
2015-11-13 15:33:59 -0800

    Moved compatibility defines to util/compat.h

--- src/simply/simply_voice.h
+++ src/simply/simply_voice.h
@@ -2,16 +2,11 @@
 
 #include "simply_msg.h"
 #include "simply.h"
+#include "util/compat.h"
 
 #include <pebble.h>
 
 #define SIMPLY_VOICE_BUFFER_LENGTH 512
-
-#ifdef PBL_SDK_2
-typedef struct DictationSession DictationSession;
-typedef struct DictationSessionStatus DictationSessionStatus;
-void dictation_session_start(DictationSession *session);
-#endif
 
 typedef struct SimplyVoice SimplyVoice;
 

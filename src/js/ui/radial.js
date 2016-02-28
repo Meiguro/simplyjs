0da98c1692866e8c43f05fb03307cb4d05f3c403
Meiguro <meiguro@meiguro.com>
2016-02-27 16:06:50 -0800

    Add Radial angle accessors

--- src/js/ui/radial.js
+++ src/js/ui/radial.js
@@ -1,11 +1,19 @@
 var util2 = require('util2');
 var myutil = require('myutil');
+var Propable = require('ui/propable');
 var StageElement = require('ui/element');
+
+var accessorProps = [
+  'radius',
+  'angleStart',
+  'angleEnd',
+];
 
 var defaults = {
   backgroundColor: 'white',
   borderColor: 'clear',
   borderWidth: 1,
+  radius: 0,
   angleStart: 0,
   angleEnd: 360,
 };
@@ -16,5 +24,7 @@ var Radial = function(elementDef) {
 };
 
 util2.inherit(Radial, StageElement);
+
+Propable.makeAccessors(accessorProps, Radial.prototype);
 
 module.exports = Radial;

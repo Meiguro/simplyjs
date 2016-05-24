06025aedca5c4ab19b481f1a6da7fc4321d43c12
Meiguro <meiguro@meiguro.com>
2016-05-23 22:36:27 -0700

    Add missing Circle radius accessor (Addresses #164)

--- src/js/ui/circle.js
+++ src/js/ui/circle.js
@@ -1,6 +1,11 @@
 var util2 = require('util2');
 var myutil = require('myutil');
+var Propable = require('ui/propable');
 var StageElement = require('ui/element');
+
+var accessorProps = [
+  'radius',
+];
 
 var defaults = {
   backgroundColor: 'white',
@@ -14,5 +19,7 @@ var Circle = function(elementDef) {
 };
 
 util2.inherit(Circle, StageElement);
+
+Propable.makeAccessors(accessorProps, Circle.prototype);
 
 module.exports = Circle;
